81dde62c75be708f56ea653947e2eb40ba9c3c5f
Enrico Sousa Gollner <enricogollner.tech@gmail.com>
2023-12-11 22:56:50 -0300

    creating models

---
+++ UIKit/ViewCode-Descomplicado/MensageApp/MensageApp/Models/MessageAllModel.swift
@@ -0,0 +1,56 @@
+//
+//  MessageAllModel.swift
+//  MensageApp
+//
+//  Created by Enrico Sousa Gollner on 11/12/23.
+//
+
+import Foundation
+
+class Message {
+    var text: String?
+    var idUser: String?
+    
+    init(dicionario: [String: Any]) {
+        self.text = dicionario["texto"] as? String
+        self.idUser = dicionario["idUser"] as? String
+    }
+}
+
+class Conversation {
+    var nome: String?
+    var lastMessage: String?
+    var idDestinatario: String?
+    
+    init(dicionario: [String: Any]) {
+        self.nome = dicionario["texto"] as? String
+        self.lastMessage = dicionario["lastMessage"] as? String
+        self.idDestinatario = dicionario["idDestinatario"] as? String
+    }
+}
+
+class User {
+    var nome: String?
+    var email: String?
+    
+    init(dicionario: [String: Any]) {
+        self.nome = dicionario["nome"] as? String
+        self.email = dicionario["email"] as? String
+    }
+}
+
+class Contact {
+    var id: String?
+    var nome: String?
+    
+    init(dicionario: [String: Any]?) {
+        self.id = dicionario?["id"] as? String
+        self.nome = dicionario?["nome"] as? String
+    }
+    
+    convenience init(id: String?, nome: String?) {
+        self.init(dicionario: nil)
+        self.id = id
+        self.nome = nome
+    }
+}
